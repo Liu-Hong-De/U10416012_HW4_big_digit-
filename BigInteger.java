e5076c0499f95928bba58f3a095081cf76497f03
Liu-Hong-De <U10416012@go.utaipei.edu.tw>
2016-03-26 14:37:13 +0800

    calculate add
    set the equation how to calculate two number adding without use BigInteger or BigDecimal and judge the number which the user input is positive or negative or zero

--- BigInteger.java
+++ BigInteger.java
@@ -6,10 +6,10 @@ public class BigInteger{
 
   private List<Integer> value;
 	
-	public BigInteger(String val) {
+	public BigInteger(String value1) {
 		
 		//take digital part
-		String v = val.charAt(0) == '-' ? val.substring(1) : val;
+		String v = value1.charAt(0) == '-' ? value1.substring(1) : value1;
 		
 		//every four characters parsed as a int
 		value = new ArrayList<>();
@@ -24,11 +24,58 @@ public class BigInteger{
 		}
 		
 		//turn into negative to complement
-		value = val.charAt(0) == '-' ? toComplement(value) : value;
+		value = value1.charAt(0) == '-' ? toComplement(value) : value;
 	}
 	
 	private BigInteger(List<Integer> value) {
 		this.value = value;
+	}
+	
+	//add
+	public BigInteger add(BigInteger t) {
+		//analyzing the number is positive or negative
+		if(isNegative(t.value)) {
+			return subtract(new BigInteger(toComplement(t.value)));
+		}
+		
+		//align the digit
+		int length = Math.max(value.size(), t.value.size());
+		List<Integer> num1 = copyOf(value, length);
+		List<Integer> num2 = copyOf(t.value, length);
+		List<Integer> result = new ArrayList<>();
+		
+		int carry = 0;
+		for(int i = 0; i < length - 1; i++) {
+			int c = num1.get(i) + num2.get(i) + carry;
+			
+			if(c < 10000) {
+				carry = 0;
+			}
+			
+			else {
+				c -= 10000;
+				carry = 1;
+			}
+			
+			result.add(c);
+		}
+		
+		if(carry == 1) {	//deal with overflow
+			
+			if(isPositive(op1)) {
+				result.add(1);
+			}
+			
+			for(int i = 0; i < 8; i++) {
+				result.add(0);	//increase eight digit
+			}
+		}
+		
+		else {		//complement, if positive number add 0, if negative number add 9999
+			result.add(isPositive(num1) ? 0 : 9999);
+		}
+		
+		return new BigInteger(result);
 	}
 	
 	//complement,deal with overflow
@@ -41,6 +88,27 @@ public class BigInteger{
 		
 		comp.set(0, comp.get(0) + 1);
 		return comp;
+	}
+	
+	//if the number which the user input is a negative number, then add 9999
+	private static boolean isNegative(List<Integer> list) {
+		return getLast(list) == 9999;
+	}
+	
+	//if the number which the number input is a positive number, then add 0
+	private static boolean isPositive(List<Integer> list) {
+		return getLast(list) == 0;
+	}
+	
+	//judge the number which the user input is or not zero
+	private static boolean isZero(List<Integer> list) {
+		for(Integer i : list) {
+			if(i != 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 	public static void main(String[] args) {
